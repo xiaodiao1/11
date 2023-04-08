f0e51cb4f6e9de535980a4b1c4a30fcab3518205
xiaodiao1 <109491399+xiaodiao1@users.noreply.github.com>
2023-04-08 22:33:25 +0800

    Add files via upload

---
+++ index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import Table from './Table'
+export default class index extends Component {
+  render() {
+    return (
+      <div>
+        <Table/>
+      </div>
+    )
+  }
+}
